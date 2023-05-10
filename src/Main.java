b8622a863b09e2f96d3684334a942564b77c49b4
Syat <titmouse99@mail.ru>
2023-05-10 03:49:54 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -11,15 +13,31 @@ public class Main {
         }
         return year;
     }
+    public static void installClients () {
+        int clientOS = 1;
+        int clientDeviceYear = LocalDate.now().getYear();
+        int IOS = 0;
+        int Android = 1;
+        if (clientOS == IOS && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        } else if (clientOS == IOS && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");}
+        if (clientOS == Android && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == Android && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
         public static void main (String[]args) {
             task1();
             task2();
         }
            public static void task1(){
-            System.out.println("Задача 3");
+            System.out.println("Задача 1");
             checkLeapYear();
     }
            public static void task2(){
-
+               System.out.println("Задача 2");
+             installClients();
            }
 }
