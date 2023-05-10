862ad22ee7b1e3ee20fb8e70c7ad0889e281f368
Syat <titmouse99@mail.ru>
2023-05-10 09:45:27 +0300

    Исправленное

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,15 @@ import java.time.LocalDate;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static int checkLeapYear () {
-        int year = 2021;
+    public static void checkLeapYear (int year) {
         if (year % 4 == 0) {
             if ((year % 100 != 0) || (year % 400 == 0)) ;
             System.out.println(year + " год - высокосный год");
         } else {
             System.out.println(year + " год - невисокосный год ");
         }
-        return year;
     }
-    public static void installClients () {
-        int clientOS = 1;
-        int clientDeviceYear = LocalDate.now().getYear();
+    public static void installClients (int clientOS , int clientDeviceYear) {
         int IOS = 0;
         int Android = 1;
         if (clientOS == IOS && clientDeviceYear <= 2015) {
@@ -28,18 +24,15 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
     }
-    public static int calculationDay () {
-        int deliveryDistance = 95;
+    public static int calculationDay (int deliveryDistance) {
         if (deliveryDistance <= 20) {
-            System.out.println("Потребуется 1 день для доставки");
+             int daysdelivery = 1;
         }
         if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется 2 дня для доставки");
+            int daydelivery = 2 ;
         }
         if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("потребуется 3 дня для доставки");
-        } else {
-            System.out.println("Доставки нет");
+            int daysdelivery = 3;
         }
         return deliveryDistance;
     }
@@ -50,14 +43,30 @@ public class Main {
         }
            public static void task1(){
             System.out.println("Задача 1");
-            checkLeapYear();
+            int year = 2021;
+               checkLeapYear (year);
     }
            public static void task2(){
                System.out.println("Задача 2");
-             installClients();
+               int clientOS = 1;
+               int clientDeviceYear = LocalDate.now().getYear();
+             installClients(clientOS, clientDeviceYear );
     }
            public static void task3(){
                System.out.println("Задача 3");
-           calculationDay();
+               int deliveryDistance = 95;
+           int days = calculationDay(deliveryDistance);
+           if (days == 1) {
+                   System.out.println("Потребуется 1 день для доставки");
+               }
+           if (days == 2)  {
+               System.out.println("Потребуется 2 дня для доставки");
+           }
+           if (days == 3) {
+               System.out.println("потребуется 3 дня для доставки");
+           }
+           else {
+               System.out.println("Доставки нет");
+           }
     }
 }
